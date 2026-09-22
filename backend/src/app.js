e71bdbb26ce5bfca92b9c33b75027f93d403cad5
Lorenzo Fernandes Aguiar <lorenzofer00@gmail.com>
2026-09-22 13:29:05 -0300

    feat: implement routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
+const employeesRoutes = require('./routes/employeesRoutes');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/employees', employeesRoutes);
 
 module.exports = app;
